154d02c1c1378af8052f5ea9d740355e18d26901
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2025-05-05 12:11:34 +0300

    Add page components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import './App.css'
 
-function App() {
+export default function App() {
+  const [advice, setAdvice] = useState("Learn from your mistakes.");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const result = await fetch("https://api.adviceslip.com/advice");
+    const data = await result.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get advice</button>
+      <Message count={count} />
     </div>
   );
 }
 
-export default App;
+function Message({ count }) {
+  return (
+    <p>
+      You have read <strong>{count}</strong> pieces of advice
+    </p>
+  );
+}
